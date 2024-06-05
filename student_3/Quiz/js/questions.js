bb32f73c8451e909c5542e86ece9b7a7a62e149e
sawmindi <129034860+sawmindi@users.noreply.github.com>
2024-06-05 15:27:56 +0530

    student_3

---
+++ student_3/Quiz/js/questions.js
@@ -0,0 +1,123 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+     question:"Where does The Elephant Gathering (billed as one of the world's greatest animal spectacles) occur every year? ",
+        answer: "Minneriya National Park",
+        options: [
+          "Yala National Park",
+          "Minneriya National Park",
+          "Udawalawe National Park",
+          "Wilpattu National Park"
+]
+  },
+
+    {
+    numb: 2,
+    question: "A word used in Sri Lanka to greet someone. What is this traditional Sri Lankan greeting?",
+    answer: "Ayubowan ",
+    options: [
+      "Jumbo",
+      "Ayubowan ",
+      "Bohoma Isthuthi ",
+      "Takahiri"
+]
+  },
+
+    {
+    numb: 3,
+    question: "Which of the following is central highland’s UNESCO world heritage site in Sri Lanka? ",
+    answer: "Yala National Park ",
+    options: [
+      "Lion Rock Sigiriya ",
+      "Dambulla Cave Temple ",
+      "Yala National Park ",
+      "Horton Plains Nuwara Eliya "
+    ]
+  },
+
+    {
+    numb: 4,
+    question: "What is the best National Park for bird watching in Sri Lanka bordering the island’s eastern coast? ",
+    answer: "Udawalawa National Park",
+    options: [
+      "Yala National Park",
+      "Udawalawa National Park",
+      "Kumana National Park",
+      "Wilpattu National Park"
+    ]
+  },
+
+    {
+    numb: 5,
+    question: "Expand UNWTO?",
+    answer: "United Nations World Tourism Organization",
+    options: [
+      "United Nations World Tourism Organization",
+      "United Nations Wide Tourism Organization",
+      "United Nations World Trade Organization",
+      "None of these"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "When is World Tourism Day?",
+    answer: "September 27",
+    options: [
+      "June 5",
+      "November 20",
+      "April 1",
+      "September 27"
+    ]
+  },
+
+    {
+    numb: 7,
+    question: "A hipster-chic east coast village known for its surfing spots that attract surfers from around the world. What is it?",
+    answer: "Arugam Bay",
+    options: [
+      "Nilaveli Beach",
+      "Arugam Bay",
+      "Marble Beach",
+      "Kalkudah Beach"
+    ]
+  },
+
+    {
+    numb: 8,
+    question: "In which Sri Lankan province is the famous Temple of the Tooth located?",
+    answer: "Central province ",
+    options: [
+      "Central province ",
+      "Southern province ",
+      "North province ",
+      "Uva province "
+    ]
+  },
+
+    {
+    numb: 9,
+    question: "What makes the Golden Temple of Dambulla so unusual?",
+    answer: "It is built in a cave",
+    options: [
+      "It is built on a rock",
+      "It is built in a cave",
+      "It is built on a wood",
+      "It is built on a cement"
+    ]
+  },
+  
+    {
+    numb: 10,
+    question: "In which Sri Lankan city is the Esala Perahera festival held? ",
+    answer: "Kandy",
+    options: [
+      "Colombo",
+      "Kataragama",
+      "Kelaniya",
+      "Kandy"
+    ]
+  },
+
+];
